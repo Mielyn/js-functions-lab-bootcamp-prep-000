abc9466852f6a439b6a0c0dbf819e82e2619d0af
Mielyn Acosta <mielyndl@gmail.com>
2018-09-04 21:27:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ function happyHolidaysTo(name){
 }
 
 function happyHolidayTo(holiday, name ){
-  return 
+  return "Happy ${holiday}, ${name}!";
 }
 
 function holidayCountdown(holiday, days){
-  return "It\'s ${days} days until ${holiday}!"'
+  return "It\'s ${days} days until ${holiday}!";
   
 }
